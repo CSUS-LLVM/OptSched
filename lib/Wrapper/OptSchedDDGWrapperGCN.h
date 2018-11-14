77acfb0e93f13280344f998ddbc39f70a9a0ded6
Austin Kerbow <amkerbow@gmail.com>
2018-11-14 00:19:06 -0800

    [DDGWrapper] Add experimental tracking of GCN subregs.
    Merge experimental GCNDDGWrapper code. Treat every sub-register
    as either an SGPR32 or VGPR32 register.

--- lib/Wrapper/OptSchedDDGWrapperGCN.h
+++ lib/Wrapper/OptSchedDDGWrapperGCN.h
@@ -4,17 +4,62 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "GCNRegPressure.h"
 #include "OptSchedDDGWrapperBasic.h"
 #include "OptimizingScheduler.h"
+#include "opt-sched/Scheduler/sched_basic_data.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/CodeGen/LiveIntervals.h"
 
 #ifndef LLVM_OPT_SCHED_DDG_WRAPPER_GCN_H
 #define LLVM_OPT_SCHED_DDG_WRAPPER_GCN_H
 
 namespace llvm {
-
 namespace opt_sched {
 
+class SubRegSet {
+private:
+  // Index subreg lanes to OptSched register numbers. Even though we can't map
+  // a LaneBitmask index to a specific sub-register, we can still accurately
+  // model the correct number of live subregs using lane mask interference.
+  SmallVector<unsigned, LaneBitmask::BitWidth> OptSchedRegMap;
+
+public:
+  using iterator = SmallVector<unsigned, LaneBitmask::BitWidth>::iterator;
+  // The max number of subregs for this virtual register.
+  unsigned Size;
+  // OptSched register type
+  int16_t Type;
+
+  iterator begin() { return OptSchedRegMap.begin(); }
+  iterator end() { return OptSchedRegMap.end(); }
+
+  SubRegSet(unsigned Size_, int16_t Type_) : Size(Size_), Type(Type_) {
+    OptSchedRegMap.resize(Size);
+  }
+  ~SubRegSet() = default;
+};
+
 class OptSchedDDGWrapperGCN : public OptSchedDDGWrapperBasic {
+private:
+  enum SubRegKind { SGPR32, VGPR32, TOTAL_KINDS };
+
+  // Map sub-registers in LLVM to a list of live subreg lanes for that register.
+  // Each live lane represents either a VGPR32 or SGPR32. In our model each live
+  // subreg lane is identified by a separate OptSched register.
+  using RegsMap = DenseMap<unsigned, std::unique_ptr<SubRegSet>>;
+  RegsMap RegionRegs;
+
+  const std::vector<llvm::SUnit> &SUnits;
+  const llvm::LiveIntervals *LIS;
+  const llvm::MachineRegisterInfo &MRI;
+
+  unsigned getRegKind(unsigned Reg) const;
+  void addLiveSubRegsAtInstr(const MachineInstr *MI, bool After);
+  void addSubRegDefs(SchedInstruction *Instr, unsigned Reg,
+                     const LaneBitmask &LiveMask);
+  void addSubRegUses(SchedInstruction *Instr, unsigned Reg,
+                     const LaneBitmask &LiveMask);
 
 public:
   OptSchedDDGWrapperGCN(llvm::MachineSchedContext *Context,
@@ -27,7 +72,6 @@ public:
 };
 
 } // end namespace opt_sched
-
 } // end namespace llvm
 
 #endif // LLVM_OPT_SCHED_DDG_WRAPPER_GCN_H
