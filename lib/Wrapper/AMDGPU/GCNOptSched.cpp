0897d75fef0af6af5b97a0d2dd9a3f5ed7f36f5f
Austin Kerbow <amkerbow@gmail.com>
2018-12-31 14:50:34 -0800

    [AMDGPU] Add iterative scheduler scaffolding
    Add GCN DAG mutations for both OptSched and GCN schedulers.
    
    Apply scheduling strategies to functions in a series of passes using GCN
    schedulers as the initial heuristic for OptSched.

---
+++ lib/Wrapper/AMDGPU/GCNOptSched.cpp
@@ -0,0 +1,132 @@
+//===- GCNOptSched.cpp - AMDGCN Combinatorial scheudler -------------------===//
+//
+// Implements a combinatorial scheduling strategy for AMDGCN.
+//
+//===----------------------------------------------------------------------===//
+
+#include "GCNOptSched.h"
+#include "AMDGPUMacroFusion.h"
+#include "GCNSchedStrategy.h"
+#include "SIMachineFunctionInfo.h"
+#include "llvm/Support/Debug.h"
+
+#define DEBUG_TYPE "optsched"
+
+using namespace llvm::opt_sched;
+
+// FIXME: Temporary, eliminate
+static cl::opt<bool>
+    GCNLimitOccWithHints("gcn-limit-occ-with-hints",
+                         cl::desc("Limit occpancy target using perf hints."),
+                         cl::init(false), cl::Hidden);
+
+static ScheduleDAGInstrs *createOptSchedGCN(MachineSchedContext *C) {
+  return new ScheduleDAGOptSchedGCN(
+      C, llvm::make_unique<GCNMaxOccupancySchedStrategy>(C));
+}
+
+// Register the machine scheduler.
+static MachineSchedRegistry
+    OptSchedMIRegistry("gcn-optsched", "Use the GCN OptSched scheduler.",
+                       createOptSchedGCN);
+
+ScheduleDAGOptSchedGCN::ScheduleDAGOptSchedGCN(
+    llvm::MachineSchedContext *C, std::unique_ptr<MachineSchedStrategy> S)
+    : ScheduleDAGOptSched(C, std::move(S)) {}
+
+void ScheduleDAGOptSchedGCN::initSchedulers() {
+  // Add DAG mutations that apply to both GCN and OptSched DAG's
+
+  addMutation(createLoadClusterDAGMutation(TII, TRI));
+  addMutation(createStoreClusterDAGMutation(TII, TRI));
+  addMutation(createAMDGPUMacroFusionDAGMutation());
+
+  // Add passes
+
+  // First
+  SchedPasses.push_back(GCNMaxOcc);
+  // Second
+  SchedPasses.push_back(OptSchedBalanced);
+}
+
+// Record scheduling regions.
+void ScheduleDAGOptSchedGCN::schedule() {
+  Regions.push_back(std::make_pair(RegionBegin, RegionEnd));
+  return;
+}
+
+// Execute scheduling passes.
+// Partially copied GCNScheduleDAGMILive::finalizeSchedule
+void ScheduleDAGOptSchedGCN::finalizeSchedule() {
+  initSchedulers();
+
+  for (const SchedPassStrategy &S : SchedPasses) {
+    MachineBasicBlock *MBB = nullptr;
+
+    for (auto &Region : Regions) {
+      RegionBegin = Region.first;
+      RegionEnd = Region.second;
+
+      if (RegionBegin->getParent() != MBB) {
+        if (MBB)
+          finishBlock();
+        MBB = RegionBegin->getParent();
+        startBlock(MBB);
+      }
+
+      unsigned NumRegionInstrs = std::distance(begin(), end());
+      enterRegion(MBB, begin(), end(), NumRegionInstrs);
+
+      // Skip empty scheduling regions (0 or 1 schedulable instructions).
+      if (begin() == end() || begin() == std::prev(end())) {
+        exitRegion();
+        continue;
+      }
+
+      LLVM_DEBUG(dbgs() << "********** MI Scheduling **********\n");
+      LLVM_DEBUG(dbgs() << MF.getName() << ":" << printMBBReference(*MBB) << " "
+                        << MBB->getName() << "\n  From: " << *begin()
+                        << "    To: ";
+                 if (RegionEnd != MBB->end()) dbgs() << *RegionEnd;
+                 else dbgs() << "End";
+                 dbgs() << " RegionInstrs: " << NumRegionInstrs << '\n');
+
+      runSchedPass(S);
+      Region = std::make_pair(RegionBegin, RegionEnd);
+      exitRegion();
+    }
+    finishBlock();
+  }
+}
+
+void ScheduleDAGOptSchedGCN::runSchedPass(SchedPassStrategy S) {
+  switch (S) {
+  case GCNMaxOcc:
+    scheduleGCNMaxOcc();
+    break;
+  case OptSchedMaxOcc:
+    scheduleOptSchedMaxOcc();
+    break;
+  case OptSchedBalanced:
+    scheduleOptSchedBalanced();
+    break;
+  }
+}
+
+void ScheduleDAGOptSchedGCN::scheduleGCNMaxOcc() {
+  if (GCNLimitOccWithHints) {
+    auto &S = (GCNMaxOccupancySchedStrategy &)*SchedImpl;
+    const auto &MFI = *MF.getInfo<SIMachineFunctionInfo>();
+    S.setTargetOccupancy(MFI.getMinAllowedOccupancy());
+  }
+
+  ScheduleDAGMILive::schedule();
+}
+
+void ScheduleDAGOptSchedGCN::scheduleOptSchedMaxOcc() {
+  // stub
+}
+
+void ScheduleDAGOptSchedGCN::scheduleOptSchedBalanced() {
+  ScheduleDAGOptSched::schedule();
+}
---
+++ lib/Wrapper/AMDGPU/GCNOptSched.h
@@ -0,0 +1,66 @@
+//===- GCNOptSched.h - AMDGCN Combinatorial scheudler -----------*- C++ -*-===//
+//
+//  OptSched combinatorial scheduler driver targeting AMDGCN.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_GCN_OPT_SCHED_H
+#define LLVM_GCN_OPT_SCHED_H
+
+#include "../OptimizingScheduler.h"
+#include "GCNRegPressure.h"
+
+namespace llvm {
+namespace opt_sched {
+
+class ScheduleDAGOptSchedGCN : public ScheduleDAGOptSched {
+private:
+  enum SchedPassStrategy {
+    GCNMaxOcc,
+    OptSchedMaxOcc,
+    OptSchedBalanced
+  };
+
+  // Vector of scheduling passes to execute.
+  SmallVector<SchedPassStrategy, 4> SchedPasses;
+
+	// Vector of regions recorded for later rescheduling
+  SmallVector<std::pair<MachineBasicBlock::iterator,
+                        MachineBasicBlock::iterator>, 32> Regions;
+
+public:
+  ScheduleDAGOptSchedGCN(llvm::MachineSchedContext *C,
+                         std::unique_ptr<MachineSchedStrategy> S);
+
+  // Rely on the machine scheduler to split the MBB into scheduling regions. In
+  // the first pass record the regions here, but don't do any actual scheduling
+  // until finalizeSchedule is called.
+  void schedule() override;
+
+  // After the scheduler is initialized and the scheduling regions have been
+  // recorded, execute the actual scheduling passes here.
+  void finalizeSchedule() override;
+
+  // Setup and select schedulers.
+  void initSchedulers();
+
+  // TODO: After we refactor OptSched scheduler options put each scheduling
+  // pass into its own class.
+
+  // Execute a scheduling pass on the function.
+  void runSchedPass(SchedPassStrategy S);
+
+  // Run GCN max occupancy scheduler.
+  void scheduleGCNMaxOcc();
+
+  // Run OptSched in RP only (max occupancy) configuration.
+	void scheduleOptSchedMaxOcc();
+
+  // Run OptSched in ILP/RP balanced mode.
+  void scheduleOptSchedBalanced();
+};
+
+} // namespace opt_sched
+} // namespace llvm
+
+#endif // LLVM_GCN_OPT_SCHED_H
