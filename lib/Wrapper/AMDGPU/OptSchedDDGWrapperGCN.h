0df2873327f26b2c63865df4e04731bc068f746b
Michael Dorst <m@mdorst.net>
2019-07-30 17:35:14 -0700

    Fix indentation in AMDGPU

--- lib/Wrapper/AMDGPU/OptSchedDDGWrapperGCN.h
+++ lib/Wrapper/AMDGPU/OptSchedDDGWrapperGCN.h
@@ -15,69 +15,69 @@
 #include "llvm/CodeGen/LiveIntervals.h"
 
 namespace llvm {
-namespace opt_sched {
+    namespace opt_sched {
 
-class SubRegSet {
-private:
-  // Index subreg lanes to OptSched register numbers. Even though we can't map
-  // a LaneBitmask index to a specific sub-register, we can still accurately
-  // model the correct number of live subregs using lane mask interference.
-  SmallVector<unsigned, 8> OptSchedRegMap;
+        class SubRegSet {
+        private:
+            // Index subreg lanes to OptSched register numbers. Even though we can't map
+            // a LaneBitmask index to a specific sub-register, we can still accurately
+            // model the correct number of live subregs using lane mask interference.
+            SmallVector<unsigned, 8> OptSchedRegMap;
 
-public:
-  using iterator = SmallVector<unsigned, 8>::iterator;
-  // The max number of subregs for this virtual register.
-  unsigned Size;
-  // OptSched register type
-  int16_t Type;
+        public:
+            using iterator = SmallVector<unsigned, 8>::iterator;
+            // The max number of subregs for this virtual register.
+            unsigned Size;
+            // OptSched register type
+            int16_t Type;
 
-  iterator begin() { return OptSchedRegMap.begin(); }
-  iterator end() { return OptSchedRegMap.end(); }
+            iterator begin() { return OptSchedRegMap.begin(); }
+            iterator end() { return OptSchedRegMap.end(); }
 
-  SubRegSet(unsigned Size_, int16_t Type_) : Size(Size_), Type(Type_) {
-    OptSchedRegMap.resize(Size);
-  }
-  ~SubRegSet() = default;
-};
+            SubRegSet(unsigned Size_, int16_t Type_) : Size(Size_), Type(Type_) {
+                OptSchedRegMap.resize(Size);
+            }
+            ~SubRegSet() = default;
+        };
 
-class OptSchedDDGWrapperGCN : public OptSchedDDGWrapperBasic {
-private:
-  // Map sub-registers in LLVM to a list of live subreg lanes for that register.
-  // Each live lane represents either a VGPR32 or SGPR32. In our model each live
-  // subreg lane is identified by a separate OptSched register.
-  using RegsMap = DenseMap<unsigned, std::unique_ptr<SubRegSet>>;
-  RegsMap RegionRegs;
-  const std::vector<llvm::SUnit> &SUnits;
-  const llvm::LiveIntervals *LIS;
-  const llvm::MachineRegisterInfo &MRI;
+        class OptSchedDDGWrapperGCN : public OptSchedDDGWrapperBasic {
+        private:
+            // Map sub-registers in LLVM to a list of live subreg lanes for that register.
+            // Each live lane represents either a VGPR32 or SGPR32. In our model each live
+            // subreg lane is identified by a separate OptSched register.
+            using RegsMap = DenseMap<unsigned, std::unique_ptr<SubRegSet>>;
+            RegsMap RegionRegs;
+            const std::vector<llvm::SUnit> &SUnits;
+            const llvm::LiveIntervals *LIS;
+            const llvm::MachineRegisterInfo &MRI;
 
-  unsigned getRegKind(unsigned Reg) const;
+            unsigned getRegKind(unsigned Reg) const;
 
-  void addLiveSubRegsAtInstr(const MachineInstr *MI, bool After);
+            void addLiveSubRegsAtInstr(const MachineInstr *MI, bool After);
 
-  void addSubRegDefs(SchedInstruction *Instr, unsigned Reg,
-                     const LaneBitmask &LiveMask, bool LiveIn = false);
+            void addSubRegDefs(SchedInstruction *Instr, unsigned Reg,
+                               const LaneBitmask &LiveMask, bool LiveIn = false);
 
-  void addSubRegUses(SchedInstruction *Instr, unsigned Reg,
-                     const LaneBitmask &LiveMask, bool LiveOut = false);
+            void addSubRegUses(SchedInstruction *Instr, unsigned Reg,
+                               const LaneBitmask &LiveMask, bool LiveOut = false);
 
-public:
-  // FIXME: Track VGPR/SGPR tuples or refactor Scheduler to use LLVM/GCN RP tracker.
-  enum SubRegKind {
-    SGPR32,
-    VGPR32,
-    TOTAL_KINDS
-  };
+        public:
+            // FIXME: Track VGPR/SGPR tuples or refactor Scheduler to use LLVM/GCN RP tracker.
+            enum SubRegKind {
+                SGPR32,
+                VGPR32,
+                TOTAL_KINDS
+            };
 
-  OptSchedDDGWrapperGCN(llvm::MachineSchedContext *Context,
-                        ScheduleDAGOptSched *DAG, OptSchedMachineModel *MM,
-                        LATENCY_PRECISION LatencyPrecision,
-                        const std::string &RegionID);
+            OptSchedDDGWrapperGCN(llvm::MachineSchedContext *Context,
+                                  ScheduleDAGOptSched *DAG, OptSchedMachineModel *MM,
+                                  LATENCY_PRECISION LatencyPrecision,
+                                  const std::string &RegionID);
 
-  void convertRegFiles() override;
-};
+            void convertRegFiles() override;
+        };
 
-} // end namespace opt_sched
+    } // end namespace opt_sched
 } // end namespace llvm
 
 #endif // LLVM_OPT_SCHED_DDG_WRAPPER_GCN_H
--- lib/Wrapper/AMDGPU/OptSchedGCNTarget.cpp
+++ lib/Wrapper/AMDGPU/OptSchedGCNTarget.cpp
@@ -25,197 +25,197 @@ using namespace llvm::opt_sched;
 static const unsigned GPRErrorMargin = 3;
 
 static unsigned getOccupancyWeight(unsigned Occupancy) {
-  if (Occupancy == 1)
-    return 100;
-  if (Occupancy == 2)
-    return 300;
-  if (Occupancy == 3)
-    return 450;
-  if (Occupancy == 4)
-    return 583;
-  if (Occupancy == 5)
-    return 708;
-  if (Occupancy == 6)
-    return 828;
-  if (Occupancy == 7)
-    return 944;
-  if (Occupancy == 8)
-    return 1059;
-  if (Occupancy == 9)
-    return 1172;
-  if (Occupancy == 10)
-    return 1283;
+    if (Occupancy == 1)
+        return 100;
+    if (Occupancy == 2)
+        return 300;
+    if (Occupancy == 3)
+        return 450;
+    if (Occupancy == 4)
+        return 583;
+    if (Occupancy == 5)
+        return 708;
+    if (Occupancy == 6)
+        return 828;
+    if (Occupancy == 7)
+        return 944;
+    if (Occupancy == 8)
+        return 1059;
+    if (Occupancy == 9)
+        return 1172;
+    if (Occupancy == 10)
+        return 1283;
 
-  llvm_unreachable("Occupancy must be between 1 and 10");
+    llvm_unreachable("Occupancy must be between 1 and 10");
 }
 
 static unsigned getAdjustedOccupancy(const GCNSubtarget *ST, unsigned VGPRCount,
                                      unsigned SGPRCount, unsigned MaxOccLDS) {
-  unsigned MaxOccVGPR = ST->getOccupancyWithNumVGPRs(VGPRCount + GPRErrorMargin);
-  unsigned MaxOccSGPR = ST->getOccupancyWithNumSGPRs(SGPRCount + GPRErrorMargin);
-  return std::min(MaxOccLDS, std::min(MaxOccVGPR, MaxOccSGPR));
+    unsigned MaxOccVGPR = ST->getOccupancyWithNumVGPRs(VGPRCount + GPRErrorMargin);
+    unsigned MaxOccSGPR = ST->getOccupancyWithNumSGPRs(SGPRCount + GPRErrorMargin);
+    return std::min(MaxOccLDS, std::min(MaxOccVGPR, MaxOccSGPR));
 }
 
 namespace {
 
-class OptSchedGCNTarget : public OptSchedTarget {
-public:
-  std::unique_ptr<OptSchedMachineModel>
-  createMachineModel(const char *ConfigPath) override {
-    return llvm::make_unique<OptSchedMachineModel>(ConfigPath);
-  }
+    class OptSchedGCNTarget : public OptSchedTarget {
+    public:
+        std::unique_ptr<OptSchedMachineModel>
+        createMachineModel(const char *ConfigPath) override {
+            return llvm::make_unique<OptSchedMachineModel>(ConfigPath);
+        }
 
-  std::unique_ptr<OptSchedDDGWrapperBase>
-  createDDGWrapper(llvm::MachineSchedContext *Context, ScheduleDAGOptSched *DAG,
-                   OptSchedMachineModel *MM, LATENCY_PRECISION LatencyPrecision,
-                   const std::string &RegionID) override {
-    return llvm::make_unique<OptSchedDDGWrapperGCN>(Context, DAG, MM,
-                                                    LatencyPrecision, RegionID);
-  }
+        std::unique_ptr<OptSchedDDGWrapperBase>
+        createDDGWrapper(llvm::MachineSchedContext *Context, ScheduleDAGOptSched *DAG,
+                         OptSchedMachineModel *MM, LATENCY_PRECISION LatencyPrecision,
+                         const std::string &RegionID) override {
+            return llvm::make_unique<OptSchedDDGWrapperGCN>(Context, DAG, MM,
+                                                            LatencyPrecision, RegionID);
+        }
 
-  void initRegion(llvm::ScheduleDAGInstrs *DAG, MachineModel *MM_) override;
+        void initRegion(llvm::ScheduleDAGInstrs *DAG, MachineModel *MM_) override;
 
-  void finalizeRegion(const InstSchedule *Schedule) override;
+        void finalizeRegion(const InstSchedule *Schedule) override;
 
-  // Returns occupancy cost with number of VGPRs and SGPRs from PRP for
-  // a partial or complete schedule.
-  InstCount getCost(const llvm::SmallVectorImpl<unsigned> &PRP) const override;
+        // Returns occupancy cost with number of VGPRs and SGPRs from PRP for
+        // a partial or complete schedule.
+        InstCount getCost(const llvm::SmallVectorImpl<unsigned> &PRP) const override;
 
-  void dumpOccupancyInfo(const InstSchedule *Schedule) const;
+        void dumpOccupancyInfo(const InstSchedule *Schedule) const;
 
-  // Revert scheduing if we decrease occupancy.
-  bool shouldKeepSchedule() override;
+        // Revert scheduing if we decrease occupancy.
+        bool shouldKeepSchedule() override;
 
-private:
-  const llvm::MachineFunction *MF;
-  SIMachineFunctionInfo *MFI;
-  ScheduleDAGOptSched *DAG;
-  const GCNSubtarget *ST;
+    private:
+        const llvm::MachineFunction *MF;
+        SIMachineFunctionInfo *MFI;
+        ScheduleDAGOptSched *DAG;
+        const GCNSubtarget *ST;
 
-  unsigned RegionStartingOccupancy;
-  unsigned RegionEndingOccupancy;
-  unsigned TargetOccupancy;
+        unsigned RegionStartingOccupancy;
+        unsigned RegionEndingOccupancy;
+        unsigned TargetOccupancy;
 
-  // Max occupancy with local memory size;
-  unsigned MaxOccLDS;
+        // Max occupancy with local memory size;
+        unsigned MaxOccLDS;
 
-  // In RP only (max occupancy) scheduling mode we should try to find
-  // a min-RP schedule without considering perf hints which suggest limiting
-  // occupancy. Returns true if we should consider perf hints.
-  bool shouldLimitWaves() const;
+        // In RP only (max occupancy) scheduling mode we should try to find
+        // a min-RP schedule without considering perf hints which suggest limiting
+        // occupancy. Returns true if we should consider perf hints.
+        bool shouldLimitWaves() const;
 
-  // Find occupancy with spill cost.
-  unsigned getOccupancyWithCost(const InstCount Cost) const;
-};
+        // Find occupancy with spill cost.
+        unsigned getOccupancyWithCost(const InstCount Cost) const;
+    };
 
-std::unique_ptr<OptSchedTarget> createOptSchedGCNTarget() {
-  return llvm::make_unique<OptSchedGCNTarget>();
-}
+    std::unique_ptr<OptSchedTarget> createOptSchedGCNTarget() {
+        return llvm::make_unique<OptSchedGCNTarget>();
+    }
 
 } // end anonymous namespace
 
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
 void OptSchedGCNTarget::dumpOccupancyInfo(const InstSchedule *Schedule) const {
-  const InstCount *PRP;
-  Schedule->GetPeakRegPressures(PRP);
+    const InstCount *PRP;
+    Schedule->GetPeakRegPressures(PRP);
 
-  unsigned SGPR32Count = PRP[MM->GetRegTypeByName("SGPR32")] + GPRErrorMargin;
-  auto MaxOccSGPR = ST->getOccupancyWithNumSGPRs(SGPR32Count);
+    unsigned SGPR32Count = PRP[MM->GetRegTypeByName("SGPR32")] + GPRErrorMargin;
+    auto MaxOccSGPR = ST->getOccupancyWithNumSGPRs(SGPR32Count);
 
-  unsigned VGPR32Count = PRP[MM->GetRegTypeByName("VGPR32")] + GPRErrorMargin;
-  auto MaxOccVGPR = ST->getOccupancyWithNumVGPRs(VGPR32Count);
-  auto Occ = std::min(std::min(MaxOccSGPR, MaxOccVGPR), MaxOccLDS);
+    unsigned VGPR32Count = PRP[MM->GetRegTypeByName("VGPR32")] + GPRErrorMargin;
+    auto MaxOccVGPR = ST->getOccupancyWithNumVGPRs(VGPR32Count);
+    auto Occ = std::min(std::min(MaxOccSGPR, MaxOccVGPR), MaxOccLDS);
 
-  dbgs() << "Estimated Max Occupancy After Scheduling: " << Occ << "\n"
-         << "Weight: " << getOccupancyWeight(Occ) << "\n"
-         << "Max Occ with LDS: " << MaxOccLDS << "\n"
-         << "Max Occ with Num SGPRs: " << MaxOccSGPR << "\n"
-         << "Max Occ with Num VGPRs: " << MaxOccVGPR << "\n";
+    dbgs() << "Estimated Max Occupancy After Scheduling: " << Occ << "\n"
+           << "Weight: " << getOccupancyWeight(Occ) << "\n"
+           << "Max Occ with LDS: " << MaxOccLDS << "\n"
+           << "Max Occ with Num SGPRs: " << MaxOccSGPR << "\n"
+           << "Max Occ with Num VGPRs: " << MaxOccVGPR << "\n";
 
-  if (MFI->getMinAllowedOccupancy() < MFI->getOccupancy())
-    dbgs() << "Occupancy is limited by perf hints:"
-           << " MemoryBound=" << MFI->isMemoryBound()
-           << " WaveLimiterHint=" << MFI->needsWaveLimiter() << "\n";
+    if (MFI->getMinAllowedOccupancy() < MFI->getOccupancy())
+        dbgs() << "Occupancy is limited by perf hints:"
+               << " MemoryBound=" << MFI->isMemoryBound()
+               << " WaveLimiterHint=" << MFI->needsWaveLimiter() << "\n";
 }
 #endif
 
 void OptSchedGCNTarget::initRegion(llvm::ScheduleDAGInstrs *DAG_,
                                    MachineModel *MM_) {
-  DAG = static_cast<ScheduleDAGOptSched *>(DAG_);
-  MF = &DAG->MF;
-  MFI =
-      const_cast<SIMachineFunctionInfo *>(MF->getInfo<SIMachineFunctionInfo>());
-  MM = MM_;
-  ST = &MF->getSubtarget<GCNSubtarget>();
-  MaxOccLDS = ST->getOccupancyWithLocalMemSize(*MF);
+    DAG = static_cast<ScheduleDAGOptSched *>(DAG_);
+    MF = &DAG->MF;
+    MFI =
+            const_cast<SIMachineFunctionInfo *>(MF->getInfo<SIMachineFunctionInfo>());
+    MM = MM_;
+    ST = &MF->getSubtarget<GCNSubtarget>();
+    MaxOccLDS = ST->getOccupancyWithLocalMemSize(*MF);
 
-  GCNDownwardRPTracker RPTracker(*DAG->getLIS());
-  RPTracker.advance(DAG->begin(), DAG->end(), nullptr);
-  const GCNRegPressure &P = RPTracker.moveMaxPressure();
-  RegionStartingOccupancy =
-      getAdjustedOccupancy(ST, P.getVGPRNum(), P.getSGPRNum(), MaxOccLDS);
-  TargetOccupancy =
-      shouldLimitWaves() ? MFI->getMinAllowedOccupancy() : MFI->getOccupancy();
+    GCNDownwardRPTracker RPTracker(*DAG->getLIS());
+    RPTracker.advance(DAG->begin(), DAG->end(), nullptr);
+    const GCNRegPressure &P = RPTracker.moveMaxPressure();
+    RegionStartingOccupancy =
+            getAdjustedOccupancy(ST, P.getVGPRNum(), P.getSGPRNum(), MaxOccLDS);
+    TargetOccupancy =
+            shouldLimitWaves() ? MFI->getMinAllowedOccupancy() : MFI->getOccupancy();
 
-  LLVM_DEBUG(dbgs() << "Region starting occupancy is "
-                    << RegionStartingOccupancy << "\n"
-                    << "Target occupancy is " << TargetOccupancy << "\n");
+            LLVM_DEBUG(dbgs() << "Region starting occupancy is "
+                              << RegionStartingOccupancy << "\n"
+                              << "Target occupancy is " << TargetOccupancy << "\n");
 }
 
 bool OptSchedGCNTarget::shouldLimitWaves() const {
-  // FIXME: Consider machine model here as well.
-  // FIXME: Return false because perf hints are not currently strong enough to
-  // use as a hard cap. Consider 'OccupancyWeight' heuristic here instead.
-  return false;
+    // FIXME: Consider machine model here as well.
+    // FIXME: Return false because perf hints are not currently strong enough to
+    // use as a hard cap. Consider 'OccupancyWeight' heuristic here instead.
+    return false;
 }
 
 unsigned OptSchedGCNTarget::getOccupancyWithCost(const InstCount Cost) const {
-  return TargetOccupancy - Cost;
+    return TargetOccupancy - Cost;
 }
 
 void OptSchedGCNTarget::finalizeRegion(const InstSchedule *Schedule) {
-  LLVM_DEBUG(dumpOccupancyInfo(Schedule));
+            LLVM_DEBUG(dumpOccupancyInfo(Schedule));
 
-  RegionEndingOccupancy = getOccupancyWithCost(Schedule->GetSpillCost());
-  // If we decrease occupancy we may revert scheduling.
-  unsigned RegionOccupancy =
-      std::max(RegionStartingOccupancy, RegionEndingOccupancy);
-  LLVM_DEBUG(if (RegionOccupancy < MFI->getOccupancy()) dbgs()
-             << "Limiting occupancy to " << RegionEndingOccupancy
-             << " waves.\n");
-  MFI->limitOccupancy(RegionOccupancy);
+    RegionEndingOccupancy = getOccupancyWithCost(Schedule->GetSpillCost());
+    // If we decrease occupancy we may revert scheduling.
+    unsigned RegionOccupancy =
+            std::max(RegionStartingOccupancy, RegionEndingOccupancy);
+            LLVM_DEBUG(if (RegionOccupancy < MFI->getOccupancy()) dbgs()
+                << "Limiting occupancy to " << RegionEndingOccupancy
+                << " waves.\n");
+    MFI->limitOccupancy(RegionOccupancy);
 }
 
 InstCount
 OptSchedGCNTarget::getCost(const llvm::SmallVectorImpl<unsigned> &PRP) const {
-  // FIXME: It's bad to asssume that the reg types for SGPR32/VGPR32 are
-  // fixed, but we avoid doing an expensive string compare here with
-  // GetRegTypeByName since updating the cost happens so often. We should
-  // replace OptSched register types completely with PSets to fix both issues.
-  auto Occ =
-      getAdjustedOccupancy(ST, PRP[OptSchedDDGWrapperGCN::VGPR32],
-                           PRP[OptSchedDDGWrapperGCN::SGPR32], MaxOccLDS);
-  // RP cost is the difference between the minimum allowed occupancy for the
-  // function, and the current occupancy.
-  return Occ >= TargetOccupancy ? 0 : TargetOccupancy - Occ;
+    // FIXME: It's bad to asssume that the reg types for SGPR32/VGPR32 are
+    // fixed, but we avoid doing an expensive string compare here with
+    // GetRegTypeByName since updating the cost happens so often. We should
+    // replace OptSched register types completely with PSets to fix both issues.
+    auto Occ =
+            getAdjustedOccupancy(ST, PRP[OptSchedDDGWrapperGCN::VGPR32],
+                                 PRP[OptSchedDDGWrapperGCN::SGPR32], MaxOccLDS);
+    // RP cost is the difference between the minimum allowed occupancy for the
+    // function, and the current occupancy.
+    return Occ >= TargetOccupancy ? 0 : TargetOccupancy - Occ;
 }
 
 bool OptSchedGCNTarget::shouldKeepSchedule() {
-  if (RegionEndingOccupancy >= RegionStartingOccupancy ||
-      RegionEndingOccupancy >= TargetOccupancy)
-    return true;
-  LLVM_DEBUG(
-      dbgs() << "Reverting Scheduling because of a decrease in occupancy from "
-             << RegionStartingOccupancy << " to " << RegionEndingOccupancy
-             << ".\n");
-  return false;
+    if (RegionEndingOccupancy >= RegionStartingOccupancy ||
+        RegionEndingOccupancy >= TargetOccupancy)
+        return true;
+            LLVM_DEBUG(
+            dbgs() << "Reverting Scheduling because of a decrease in occupancy from "
+                   << RegionStartingOccupancy << " to " << RegionEndingOccupancy
+                   << ".\n");
+    return false;
 }
 
 namespace llvm {
-namespace opt_sched {
+    namespace opt_sched {
 
-OptSchedTargetRegistry OptSchedGCNTargetRegistry("amdgcn",
-                                                 createOptSchedGCNTarget);
+        OptSchedTargetRegistry OptSchedGCNTargetRegistry("amdgcn",
+                                                         createOptSchedGCNTarget);
 
-} // namespace opt_sched
+    } // namespace opt_sched
 } // namespace llvm
