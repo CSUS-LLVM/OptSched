85e1710331ac3d706428cda41ceeb1701e8b6818
Justin Bassett <jbassett271@gmail.com>
2020-09-21 18:16:11 -0700

    Add INFO: prefixes, bypass deleted lines

--- util/misc/json2infolog.py
+++ util/misc/json2infolog.py
@@ -31,35 +31,35 @@ def produce(s, cont=identity):
 discard = produce(None)
 
 TR_TABLE = {
-    'StaticLowerBoundDebugInfo': format('DAG {name} spillCostLB {spill_cost_lb} scFactor {sc_factor} lengthLB {length_lb} lenFactor {len_factor} staticLB {static_lb}'),
-    'Enumerating': format('Enumerating at target length {target_length}'),
-    'ScheduleVerifiedSuccessfully': discard, # produce('Schedule verified successfully'),
-    'ProcessDag': discard, # format('Processing DAG {name} with {num_instructions} insts and max latency {max_latency}.'),
-    'HeuristicResult': discard, # format('The list schedule is of length {length} and spill cost {spill_cost}. Tot cost = {cost}'),
-    'CostLowerBound': discard, # format('Lower bound of cost before scheduling: {cost}'),
-    'BypassZeroTimeLimit': discard, # format('Bypassing optimal scheduling due to zero time limit with cost {cost}'),
-    'HeuristicScheduleOptimal': format('The initial schedule of length {length} and cost {cost} is optimal.'),
+    'StaticLowerBoundDebugInfo': format('INFO: DAG {name} spillCostLB {spill_cost_lb} scFactor {sc_factor} lengthLB {length_lb} lenFactor {len_factor} staticLB {static_lb}'),
+    'Enumerating': format('INFO: Enumerating at target length {target_length}'),
+    'ScheduleVerifiedSuccessfully': discard, # produce('INFO: Schedule verified successfully'),
+    'ProcessDag': discard, # format('INFO: Processing DAG {name} with {num_instructions} insts and max latency {max_latency}.'),
+    'HeuristicResult': discard, # format('INFO: The list schedule is of length {length} and spill cost {spill_cost}. Tot cost = {cost}'),
+    'CostLowerBound': discard, # format('INFO: Lower bound of cost before scheduling: {cost}'),
+    'BypassZeroTimeLimit': discard, # format('INFO: Bypassing optimal scheduling due to zero time limit with cost {cost}'),
+    'HeuristicScheduleOptimal': format('INFO: The initial schedule of length {length} and cost {cost} is optimal.'),
     'BestResult': discard,
         # translate_args(
-        #     format('Best schedule for DAG {name} has cost {cost} and length {length}. The schedule is {optimal}'),
+        #     format('INFO: Best schedule for DAG {name} has cost {cost} and length {length}. The schedule is {optimal}'),
         #     optimal=lambda opt: 'optimal' if opt else 'not optimal'
         # ),
-    'SlilStats': discard, # format('SLIL stats: DAG {name} static LB {static_lb} gap size {gap_size} enumerated {is_enumerated} optimal {is_optimal} PERP higher {is_perp_higher}'),
-    'NodeExamineCount': format('Examined {num_nodes} nodes.'),
-    'DagSolvedOptimally': discard, # format('DAG solved optimally in {solution_time} ms with length={length}, spill cost = {spill_cost}, tot cost = {total_cost}, cost imp={cost_improvement}.'),
-    'DagTimedOut': format('DAG timed out with length={length}, spill cost = {spill_cost}, tot cost = {total_cost}, cost imp={cost_improvement}.'),
+    'SlilStats': discard, # format('INFO: SLIL stats: DAG {name} static LB {static_lb} gap size {gap_size} enumerated {is_enumerated} optimal {is_optimal} PERP higher {is_perp_higher}'),
+    'NodeExamineCount': format('INFO: Examined {num_nodes} nodes.'),
+    'DagSolvedOptimally': discard, # format('INFO: DAG solved optimally in {solution_time} ms with length={length}, spill cost = {spill_cost}, tot cost = {total_cost}, cost imp={cost_improvement}.'),
+    'DagTimedOut': format('INFO: DAG timed out with length={length}, spill cost = {spill_cost}, tot cost = {total_cost}, cost imp={cost_improvement}.'),
     'HeuristicLocalRegAllocSimulation': format(
         dedent('''\
-        OPT_SCHED LOCAL RA: DAG Name: {dag_name} ***heuristic_schedule*** Number of spills: {num_spills}
-        Number of stores {num_stores}
-        Number of loads {num_loads}''')),
+        INFO: OPT_SCHED LOCAL RA: DAG Name: {dag_name} ***heuristic_schedule*** Number of spills: {num_spills}
+        INFO: Number of stores {num_stores}
+        INFO: Number of loads {num_loads}''')),
     'BestLocalRegAllocSimulation': format(
         dedent('''\
-        OPT_SCHED LOCAL RA: DAG Name: {dag_name} Number of spills: {num_spills}
-        Number of stores {num_stores}
-        Number of loads {num_loads}''')),
+        INFO: OPT_SCHED LOCAL RA: DAG Name: {dag_name} Number of spills: {num_spills}
+        INFO: Number of stores {num_stores}
+        INFO: Number of loads {num_loads}''')),
     'LocalRegAllocSimulationChoice': discard,
-    'PassFinished': discard, # lambda log: ('End of first pass through\n', 'End of second pass through')[log['num']],
+    'PassFinished': discard, # lambda log: ('INFO: End of first pass through\n', 'INFO: End of second pass through')[log['num']],
 }
 
 
@@ -72,7 +72,9 @@ def translate(infile, outfile):
 
         parsed = json.loads(line.split(' ', 1)[1])
         tr = TR_TABLE[parsed['event_id']]
-        print(tr(parsed), file=outfile)
+        result = tr(parsed)
+        if result is not None:
+            print(result, file=outfile)
 
 
 for inpath, outpath in zip(args.input, args.output):
