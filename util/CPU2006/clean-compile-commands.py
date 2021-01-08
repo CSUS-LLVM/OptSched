8107a9b3e63d8ded35107c0c8a468c002c9a1062
Justin Bassett <jbassett271@gmail.com>
2021-01-08 14:51:29 -0800

    Handle INFO logs and interrupted tags
    From the PR review, it was pointed out that log tags (EVENT:) can be
    interrupted by compilation commands, so we probably want to handle such
    cases as well. It turns out that SPEC's compilation commands are
    intended to always appear at the beginning of a line, so matching just
    for the first character of a log tag would be unlikely to result in
    false positives.
    
    It was also pointed out that INFO logs can also be interrupted, so it
    makes sense to fix them as well. This is especially true because of the
    way that we split logfiles: we split off of
    'INFO: ********** Opt Scheduling **********'.
    If this string was interrupted by a compilation command, suddenly we'd
    merge multiple blocks together in our analysis.

--- util/CPU2006/clean-compile-commands.py
+++ util/CPU2006/clean-compile-commands.py
@@ -10,14 +10,20 @@ files = parser.parse_args().files
 RE_AFTER_FUNCTION = re.compile(r'\*{20,}\nFunction:.*?\*{20,}\n', re.DOTALL)
 RE_BUGGED_COMPILE_COMMAND = re.compile(
     r'''
-    EVENT:\ \{.*?   # If we are in an EVENT log
+    # Compilation commands will always appear at the beginning of a line if things happened correctly.
+    # We're trying to fix it when it doesn't happen correctly.
+    ^(?: # (Non-capturing)
+        E       # Inside an EVENT: log, but possible happening mid-word (EV/usr/bin/clang++ ...)
+        | I     # Inside an INFO: log
+    )
+    .*?
     (
         # When we see any of the bugged compilation commands,
         (/.*?/[cf]lang.*\n) # clang, clang++, flang
         | (specperl\ /.*\n) # specperl commands
     )   # then we want to match the command and move it to the end.
     ''',
-    re.VERBOSE)
+    re.VERBOSE | re.MULTILINE)
 
 for file in files:
     with open(file, 'r') as f:
