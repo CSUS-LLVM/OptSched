93a273c7413b0f83f9684ad4476c0d6ec2a66425
Justin Bassett <jbassett271@gmail.com>
2021-04-16 10:11:07 -0700

    Remove outdated scripts
    These used the old model.

--- util/gt_analysis/plaidbench_gt_cost.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from gt_cost import RpGtCost
-from analyze.script import *
-
-run(RpGtCost, _0, _1, benchsuite='plaidml') \
-    .keep_if(lambda blk: 'PassFinished' in blk) \
-    .keep_if(lambda blk: blk.single('PassFinished')['num'] == 1)
