97e82dab522f8fd62b86bb8796aedc612e63bb4b
Justin Bassett <jbassett271@gmail.com>
2021-04-29 17:19:41 -0700

    Improve load_logs script
    Drops into an interpreter even if not run with python -i

--- util/misc/load_logs.py
+++ util/misc/load_logs.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
-# Intended to be used with python -i to load the logs to then be worked on in the REPL
-
 import argparse
+import sys
 import analyze
+
+__INTERACTIVE = bool(getattr(sys, 'ps1', sys.flags.interactive))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('logs', nargs='+', help='The logs to analyze')
     args = analyze.parse_args(parser, 'logs')
     logs = args.logs
+
+    if __INTERACTIVE:
+        print('Parsed logs into variable `logs`')
+    else:
+        import code
+        code.interact(banner='Parsed logs into variable `logs`', exitmsg='', local={'logs': logs})
