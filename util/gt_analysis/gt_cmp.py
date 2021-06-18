331b4a98074a3a3d8d2ea0cddeb958af5903048a
Justin Bassett <jbassett271@gmail.com>
2021-06-17 17:14:40 -0700

    Change metrics again

--- util/gt_analysis/gt_cmp.py
+++ util/gt_analysis/gt_cmp.py
@@ -7,6 +7,7 @@ from analyze import Block, Logs, utils
 from analyze.lib import block_stats, compile_times
 
 sched_time = compile_times.sched_time
+total_compile_time_seconds = compile_times.total_compile_time_seconds
 
 
 def blocks_enumerated_optimally(blocks):
@@ -54,7 +55,7 @@ def elapsed_before_enumeration_for_blk(blk: Block) -> int:
 
 def enum_time_for_blk(blk: Block) -> int:
     if 'DagSolvedOptimally' not in blk:
-        return 0
+        return blk.single('DagTimedOut')['time'] - blk['Enumerating'][0]['time']
     return blk.single('DagSolvedOptimally')['time'] - blk['Enumerating'][0]['time']
 
 
@@ -69,24 +70,40 @@ def is_improved(before: Block, after: Block):
 def compute_stats(nogt: Logs, gt: Logs):
     TOTAL_BLOCKS = utils.count(nogt)
 
+    nogt_all, gt_all = nogt, gt
+
+    NUM_PROVED_OPTIMAL_WITHOUT_ENUMERATING = utils.count(utils.zipped_keep_blocks_if(
+        nogt, gt, pred=lambda a, b: block_stats.is_enumerated(a) and not block_stats.is_enumerated(b))[0])
     nogt, gt = utils.zipped_keep_blocks_if(
         nogt, gt, pred=block_stats.is_enumerated)
 
+    nogt_opt, gt_opt = utils.zipped_keep_blocks_if(nogt, gt, pred=lambda b: 'DagSolvedOptimally' in b)
+
     result = {
         'Total Blocks in Benchsuite': TOTAL_BLOCKS,
-        'Num Blocks with GT applied': utils.count(nogt),
-        'Num Nodes Examined (opt. blocks only) (No GT)': utils.sum_stat_for_all(block_stats.nodes_examined_for_blk, nogt),
-        'Num Nodes Examined (opt. blocks only) (GT)': utils.sum_stat_for_all(block_stats.nodes_examined_for_blk, gt),
+        'Num Blocks enumerated with & without GT': utils.count(nogt),
+        'Num Blocks proved optimal just by GT': NUM_PROVED_OPTIMAL_WITHOUT_ENUMERATING,
 
+        'Total Compile Time (s) (all benchsuite) (No GT)': total_compile_time_seconds(nogt_all),
+        'Total Compile Time (s) (all benchsuite) (GT)': total_compile_time_seconds(gt_all),
         'Total Sched Time (No GT)': sched_time(nogt),
         'Total Sched Time (GT)': sched_time(gt),
         'Enum Time (No GT)': utils.sum_stat_for_all(enum_time_for_blk, nogt),
         'Enum Time (GT)': utils.sum_stat_for_all(enum_time_for_blk, gt),
         'Total GT Time': utils.sum_stat_for_all(total_gt_elapsed_for_blk, gt),
+
+        'Total Sched Time (opt. blks only) (No GT)': sched_time(nogt_opt),
+        'Total Sched Time (opt. blks only) (GT)': sched_time(gt_opt),
+        'Enum Time (opt. blocks only) (No GT)': utils.sum_stat_for_all(enum_time_for_blk, nogt_opt),
+        'Enum Time (opt. blocks only) (GT)': utils.sum_stat_for_all(enum_time_for_blk, gt_opt),
+
         'Block Cost - Relative (No GT)': utils.sum_stat_for_all(block_stats.block_relative_cost, nogt),
         'Block Cost - Relative (GT)': utils.sum_stat_for_all(block_stats.block_relative_cost, gt),
         'Block Cost (No GT)': utils.sum_stat_for_all(block_stats.block_cost, nogt),
         'Block Cost (GT)': utils.sum_stat_for_all(block_stats.block_cost, gt),
+
+        'Num Nodes Examined (opt. blocks only) (No GT)': utils.sum_stat_for_all(block_stats.nodes_examined_for_blk, nogt_opt),
+        'Num Nodes Examined (opt. blocks only) (GT)': utils.sum_stat_for_all(block_stats.nodes_examined_for_blk, gt_opt),
 
         'Num Timeout Unimproved (No GT)': utils.count(blk for blk in nogt
                                                       if block_stats.is_timed_out(blk)
