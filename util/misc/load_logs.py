cd53602db318635fd62654e5b7c4ee7219a8f766
Justin Bassett <jbassett271@gmail.com>
2021-08-24 11:38:35 -0700

    Encourage running load_logs with python -i
    Without it, there was no interpreter history. With it, we get Python's
    interactive interpreter history.

--- util/misc/load_logs.py
+++ util/misc/load_logs.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
-
 import argparse
-import sys
 import analyze
-
-__INTERACTIVE = bool(getattr(sys, 'ps1', sys.flags.interactive))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,8 +7,4 @@ if __name__ == '__main__':
     args = analyze.parse_args(parser, 'logs')
     logs = args.logs
 
-    if __INTERACTIVE:
-        print('Parsed logs into variable `logs`')
-    else:
-        import code
-        code.interact(banner='Parsed logs into variable `logs`', exitmsg='', local={'logs': logs})
+    print('Parsed logs into variable `logs`')
